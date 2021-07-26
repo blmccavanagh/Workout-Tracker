972788c037217ad0779e419d8519e2ceb51995c2
Bridget Louise McCavanagh <blmccavanagh@gmail.com>
2021-07-26 11:30:17 +0800

    Get route to '/workouts' created.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,33 @@
 const router = require("express").Router();
+const { Workout } = require('../models');
+
+// CRUD
+// Create, Read, Update, Delete
+
+// POST, GET, PUT/PATCH, DELETE
+
+// GET downloading and reading HTML resources
+// api/json list (such as api/users returns a list of users as an array)
+// api/json response specific eg /api/users/5 returns user #5
+
+router.get('/workouts', async (req, res) => {
+    try {
+        const workoutData = await Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: {
+                        $sum: "$exercises.duration",
+                    }
+                }
+            }
+        ]);
+        res.status(200).json(workoutData);
+    } catch (err) {
+        res.status(500).json({errors: err});
+    }
+});
+
+// api/workouts/range - GET request
+// likely to be reading a list of resources
 
 module.exports = router;
