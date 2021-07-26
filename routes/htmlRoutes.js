ded535efc1dc06cfcc306e6aed5fda56c6806162
Bridget Louise McCavanagh <blmccavanagh@gmail.com>
2021-07-26 11:32:03 +0800

    HTML routes created.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,16 @@
 const router = require("express").Router();
+const path = require("path");
+
+router.get('/exercise', (req, res) => {
+    // dirname is current directory
+    const dirPath = path.join(__dirname, '..', 'public', 'exercise.html');
+    res.status(200).sendFile(dirPath);
+});
+
+router.get('/stats', (req, res) => {
+    // dirname is current directory
+    const dirPath = path.join(__dirname, '..', 'public', 'stats.html');
+    res.status(200).sendFile(dirPath);
+});
 
 module.exports = router;
